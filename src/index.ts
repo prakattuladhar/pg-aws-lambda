e3b98ec30474694fb38341027d004e40f5c6ea11
Prakat Tuladhar <prakattuladhar@gmail.com>
2021-09-03 19:26:27 -0500

    exports look good

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Pg from "./Pg";
 const db = new Pg();
-
 //When this file is imported a pool is created by default providing a global pool for the lambda
-// export const query = db.query;
-// const obj= {
-//   query: db.query
-// }
-// module.exports = db;
-export default { query: db.query };
+export const query = db.query.bind(db);
+export const setConfig = db.setConfig.bind(db);
+export default Pg;
+
+
+// module.exports = Pg;
+// exports = module.exports;
+// exports.query = db.query.bind(db);
--- test.js
+++ test.js
@@ -6,10 +6,13 @@ process.env.PGPORT = 5432;
 process.env.PG_DEBUG = "true";
 
 // const Pg = require("./dist/index");
-const db = require("./dist/index");
+// const Pg = require("./dist/index");
+const { query } = require("./dist/index");
+const Pg = require("./dist/index")
 // new Pg().query("select Now()").then(result=>{})
-
-db.default.query("select NOW()")
+// console.log(db)
+console.log(Pg);
+query("select NOW()")
   .then((test) => {
     console.log(test);
   })
